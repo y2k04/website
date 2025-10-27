fb2113e1b51f0d68ad11cc79aa7f8ac49fa9a718
Y2K4 <29883287+y2k04@users.noreply.github.com>
2025-10-27 15:47:22 +1100

    Revamp UI and clean up code
    This was well needed. Looks so much better, and works better on mobile!

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,33 @@
-if (location.href == `${location.origin}/` || location.href.endsWith("/index.html") || location.href.endsWith("/index")) {
-var tmp = undefined;
-var repos = [];
-var gists = [];
+function remToPx(rem) {    
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
 
-(async () => {
-    tmp = await fetch('https://api.github.com/users/y2k04/repos?sort=pushed').then(response => response.json());
-})().then(()=>{
-    for(var i = 0; i < tmp.length && repos.length != 5; i++) {
-        if(!tmp[i].fork)
-            repos.push(tmp[i]);
-    }
+function changeOnSize() {
+    if (document.body.clientWidth < 500)
+        document.querySelector(".menu").className = document.querySelector(".menu").className.replace("vertical", "horizontal");
+    else
+        document.querySelector(".menu").className = document.querySelector(".menu").className.replace("horizontal", "vertical");
+}
 
-    for (var i = 0; i < repos.length; i++) {
-        var el = document.createElement("div");
-        el.innerHTML = `<div class="content"><a class="header" href="${repos[i].html_url}">${repos[i].name}</a><div class="description">${repos[i].description}</div></div>`;
-        el.className = "item";
-        document.getElementById("latestRepos").appendChild(el);
-    }
-});
+window.onresize = changeOnSize;
+changeOnSize();
 
-tmp = null;
+if (location.pathname == "/" || location.pathname.startsWith("/index")) {
+    (async () => await fetch('https://api.github.com/users/y2k04/repos?sort=pushed').then(r => r.json()).then(repos => {
+        for (var i = 0; i < 5; i++) {
+            var el = document.createElement("div");
+            el.innerHTML = `<div class="content"><a class="header" href="${repos[i].html_url}">${repos[i].name}</a><div class="description">${repos[i].description}</div></div>`;
+            el.className = "item";
+            document.getElementById("latestRepos").appendChild(el);
+        }
+    }))();
 
-(async () => {
-    gists = await fetch('https://api.github.com/users/y2k04/gists?per_page=5').then(response => response.json());
-})().then(()=>{
-    for (var i = 0; i < gists.length; i++) {
-        var el = document.createElement("div");
-        el.innerHTML = `<div class="content"><a class="header" href="${gists[i].url}">${Object.keys(gists[i].files)[0]}</a><div class="description">${gists[i].description}</div></div>`;
-        el.className = "item";
-        document.getElementById("latestGists").appendChild(el);
-    }
-});
+    (async () => await fetch('https://api.github.com/users/y2k04/gists?per_page=5').then(r => r.json()).then(gists => {
+        for (var i = 0; i < 5; i++) {
+            var el = document.createElement("div");
+            el.innerHTML = `<div class="content"><a class="header" href="${gists[i].url}">${Object.keys(gists[i].files)[0]}</a><div class="description">${gists[i].description}</div></div>`;
+            el.className = "item";
+            document.getElementById("latestGists").appendChild(el);
+        }
+    }))();
 }
